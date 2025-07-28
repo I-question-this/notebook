23e143eaa86de98f8f9011c1c8322b34dc439a75
Tyler Westland <tylerofthewest@gmail.com>
2025-07-28 18:13:11 -0500

    Corrects class name to match file name

--- lib/Migration/Version010100Date20250727223210.php
+++ lib/Migration/Version010100Date20250727223210.php
@@ -10,7 +10,7 @@ use OCP\DB\Types;
 use OCP\Migration\IOutput;
 use OCP\Migration\SimpleMigrationStep;
 
-class Version010100Date20230524153814 extends SimpleMigrationStep {
+class Version010100Date20250727223210 extends SimpleMigrationStep {
 
 	/**
 	 * @param IOutput $output
