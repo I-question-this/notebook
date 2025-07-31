b145b257ca7cd8b08e0bfe1fc24a98907c6e5c0a
Tyler Westland <tylerofthewest@gmail.com>
2025-07-30 19:56:04 -0500

    Automated tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,6 @@
 <?php
 
-declare(strict_types=1);
-
 require_once __DIR__ . '/../../../tests/bootstrap.php';
-require_once __DIR__ . '/../vendor/autoload.php';
 
-\OC_App::loadApp(OCA\Notebook\AppInfo\Application::APP_ID);
+\OC_App::loadApp(OCA\NoteBook\AppInfo\Application::APP_ID);
 OC_Hook::clear();
